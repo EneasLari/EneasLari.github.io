34e083e05c4835b50ba9cb8176849f0d124c4ab9
eneaslar <eneaslari@gmail.com>
2020-12-21 17:34:23 +0200

    trying effevt

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1,12 +1,12 @@
-(function($) {
+(function ($) {
   "use strict"; // Start of use strict
 
   // Floating label headings for the contact form
-  $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+  $("body").on("input propertychange", ".floating-label-form-group", function (e) {
     $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
-  }).on("focus", ".floating-label-form-group", function() {
+  }).on("focus", ".floating-label-form-group", function () {
     $(this).addClass("floating-label-form-group-with-focus");
-  }).on("blur", ".floating-label-form-group", function() {
+  }).on("blur", ".floating-label-form-group", function () {
     $(this).removeClass("floating-label-form-group-with-focus");
   });
 
@@ -19,7 +19,7 @@
     $(window).on('scroll', {
         previousTop: 0
       },
-      function() {
+      function () {
         var currentTop = $(window).scrollTop();
         //check if user is scrolling up
         if (currentTop < this.previousTop) {
@@ -41,59 +41,58 @@
 })(jQuery); // End of use strict
 
 //MY SCRIPTS FROM NOW ON
+function typeWriter(parentoftext, text, txtspeed, delay) {
+  ChangeCharacterVisibility(0, parentoftext, text, 0, "hidden")
+  setTimeout(function () {
+    ChangeCharacterVisibility(0, parentoftext, text, txtspeed, "visible");
+  }, delay)
+}
+
+function ChangeCharacterVisibility(i, parentoftext, text, textspeed, visibility) {
+  if (textspeed == 0 && i < text.length) {
+    if (i == 0) {
+      parentoftext.innerHTML = ""
+    }
+    parentoftext.innerHTML += "<span style='visibility:" + visibility + "'>" + text.charAt(i) + "</span>";
+    i++;
+    ChangeCharacterVisibility(i, parentoftext, text, 0, visibility)
+
+  } else if (textspeed > 0 && parentoftext.hasChildNodes() && i < parentoftext.childNodes.length) {
+
+    setTimeout(function () {
+      parentoftext.childNodes[i].style.visibility = "visible"
+      i++;
+      ChangeCharacterVisibility(i, parentoftext, text, textspeed, visibility)
+    }, textspeed);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function (event) {
-  // array with texts to type in typewriter
-  var dataText = ["Amsterdam.", "Full Service.", "Webdevelopment.", "Wij zijn Occhio!"];
-  var textToAnimate = document.getElementById("heading-title").innerText;
-  // type one text in the typwriter
-  // keeps calling itself until the text is finished
-  function typeWriter(text, i, fnCallback) {
-    // chekc if text isn't finished yet
-    if (i < (text.length)) {
-      // add next character to h1
-      document.querySelector("h2").innerHTML = text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+  var speed = 50;
 
-      // wait for a while and call this function again for next character
-      setTimeout(function () {
-        typeWriter(text, i + 1, fnCallback)
-      }, 100);
-    }
-    // text finished, call callback if there is a callback function
-    else if (typeof fnCallback == 'function') {
-      // call callback after timeout
-      setTimeout(fnCallback, 700);
-    }
-  }
-  // start a typewriter animation for a text textToAnimate
-  function StartTextAnimationOnText() {
-    if (typeof textToAnimate == 'undefined') {
-      setTimeout(function () {
-        StartTextAnimation(0);
-      }, 20000);
-    }
-    typeWriter(textToAnimate, 0);
-    // check if dataText[i] exists
-  }
-  // start a typewriter animation for a text in the dataText array
-  function StartTextAnimation(i) {
-    if (typeof dataText[i] == 'undefined') {
-      setTimeout(function () {
-        StartTextAnimation(0);
-      }, 20000);
-    }
-    // check if dataText[i] exists
-    if (i < dataText[i].length) {
-      // text exists! start typewriter animation
-      typeWriter(dataText[i], 0, function () {
-        // after callback (and whole text has been animated), start next text
-        StartTextAnimation(i + 1);
-      });
-    }
-  }
-  // start the text animation
-  //StartTextAnimation(0);
+  var parentelement = document.getElementById("heading-title-animation")
+  var txt = parentelement.innerText;
+  typeWriter(parentelement, txt, speed, 2000)
+});
 
-  // start the text animation ON textToAnimate
-  StartTextAnimationOnText();
+var observer = new IntersectionObserver(function (entries) {
+  for (let i = 0; i < entries.length; i++) {
+    if (entries[i].isIntersecting === true) {
+      if (!entries[i]['target'].classList.contains("scaled")) {
+        entries[i]['target'].classList.add("scaled")
+      }
+    } else{
+      if (entries[i]['target'].classList.contains("scaled")) {
+        entries[i]['target'].classList.remove("scaled")
+      }
 
+    }
+
+  }
+}, {
+   rootMargin:"-49.9% 0px -50% 0px"
+});
+
+Array.from(document.getElementsByClassName("observed")).forEach(element => {
+  observer.observe(element);
 });
